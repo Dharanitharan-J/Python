d117d587ba79ef5e6b555d018a03bd9172ae8ee0
Dharanitharan-J <130481132+Dharanitharan-J@users.noreply.github.com>
2023-06-07 12:32:35 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,10 @@
+a=int(input("Enter low value : "))
+b=int(input("Enter high value : "))
+
+for i in range(a,b+1):
+    if i>1:
+        for j in range(2,i):
+            if(i%j)==0:
+                break
+        else:
+            print(i)
